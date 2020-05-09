0a595487d5eb364a593c2208e2e40b9eeee3292b
unknown <KalebJohnson45@yahoo.com>
2020-05-08 19:47:12 -0500

    Finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-
+import { Route, Link  } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
+
 
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
@@ -11,8 +14,21 @@ const App = () => {
 
   return (
     <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <SavedList list={savedList}/>
+    <Route exact path="/" component={MovieList}/>
+    <Route path="/movies/:id" component={Movie}
+        render={props => {
+          return (
+            <movies
+            items={savedList}
+            
+            {...props}
+          
+        />
+      );
+     }}
+     />
+          
     </div>
   );
 };
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Router, Link  } from 'react-router-dom';
 
 const MovieCard = props => {
-  return;
+  return   (      
+  <div>
+  <Link to="/movies/id">Home</Link>
+  </div>
+  );
 };
 
 export default MovieCard;
